a19873ea1a364bb79b76276ebf357298a6e8075e
Canti17 <cantisani.1707633@studenti.uniroma1.it>
2018-04-18 14:47:06 +0200

    Errore in compilazione da capire

--- disastrOS_semopen.c
+++ disastrOS_semopen.c
@@ -6,6 +6,65 @@
 #include "disastrOS_semaphore.h"
 #include "disastrOS_semdescriptor.h"
 
+//#include "linked_list.h"			//aggiungo il file linked_list.h per le funzioni Insert e Detach
+#include "disastrOS_globals.h"     //aggiungo il file globals.h per le variabili globali , tra le quali semaphores_list
+#include "disastrOS_constants.h"	//aggiungo il file constants.h per i messaggi di errore
+
 void internal_semOpen(){
-  // do stuff :)
+  
+  int id = running->syscall_args[0];    		//Primo Argomento sem_open
+  int value = running->syscall_args[1]; 		//Secondo Argomento sem_open
+  
+  
+  Semaphore* sem = SemaphoreList_byId(&semaphores_list, id);  	//Cerco il semaforo in semaphores_list con il suo ID.
+  
+  
+  if (value < 0){   			//Semaforo già in uso	  
+	 //running->syscall_retvalue = MESS ERRORE
+  }
+  
+  
+  if(!sem){
+	  sem = Semaphore_alloc(id, value);   			//Se il semaforo non esiste in semaphores_list, lo alloco.
+	  }
+	  if(!sem){
+		  //ERRORE ALLOCAZIONE CON MESSAGGIO DI ERRORE
+		  return;
+		  }	  
+		  
+	List_Insert(&semaphores_list, semaphores_list.last, (ListItem*) sem);   //Aggiungo il semaforo a semaphores_list
+	
+	
+	
+	int fd = running->last_sem_fd;   //Ultimo sem_fd che si può usare
+	
+	SemDescriptor* sem_desc = SemDescriptor_alloc(fd, sem, running);  //Alloco il SemDescriptor che si rifersce a sem
+	if(!sem_desc){
+		
+		//running->syscall_retvalue = Mess Errore
+		return;
+	}
+	
+	running->last_sem_fd++;  //Aggiorno il valore di last_sem_fd per il prossimo semaforo.
+	
+	List_Insert(&running->sem_descriptors, running->sem_descriptors.last, (ListItem*) sem_desc);  //Aggiungo il descrittore allocato alla lista dei descrittori Sem_descriptors
+	
+	
+	
+	SemDescriptorPtr* sem_descptr = SemDescriptorPtr_alloc(sem_desc);  //Alloco il SemDescriptorPtr per le risorse
+	if(!sem_descptr){
+		//Mess Errore	
+		return;		
+					}
+					
+	sem_descptr->descriptor = sem_desc;  //Aggiungo il descrittore sem_desc nella struttura di sem_descptr
+					
+					
+	List_Insert(&sem, sem->descriptors.last, sem_descptr);  //Aggiungo alla ListHead Descriptors di sem il descrittore ptr.
+	
+	
+	
+	running->syscall_retvalue = sem_desc->fd;   //Restituisco l' fd del descrittore sem_desc che si riferisce al nostro semaforo.
+	return;
+					
 }
