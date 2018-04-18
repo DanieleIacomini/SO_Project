c415f58481ab3142243533db38a426d6223c0575
Canti17 <cantisani.1707633@studenti.uniroma1.it>
2018-04-18 15:46:56 +0200

    mancano messaggi d'errore e errore sulla List_Detach

--- disastrOS_semclose.c
+++ disastrOS_semclose.c
@@ -5,7 +5,59 @@
 #include "disastrOS_syscalls.h"
 #include "disastrOS_semaphore.h"
 #include "disastrOS_semdescriptor.h"
+#include "disastrOS_globals.h"     //aggiungo il file globals.h per le variabili globali , tra le quali semaphores_list
+#include "disastrOS_constants.h"	//aggiungo il file constants.h per i messaggi di errore
+#include "linked_list.h"				//per la List_Detach
+
 
 void internal_semClose(){
-  // do stuff :)
+	  int check;									//Variabile per controllo errori
+	  int fd = running->syscall_args[0];    		//Argomento sem_close
+	  
+	  SemDescriptor* sem_desc = SemDescriptorList_byFd(&semaphores_list, fd); //Cerco il SemDescriptor tramite il suo fd
+	  if(!sem_desc){//MESS ERRORE
+	  }
+	  
+	  
+	  
+	  Semaphore* sem = sem_desc-> semaphore; 			//Prendo il semaforo corrispondente al descrittore Sem_Desc
+	  if(!sem){ //MESS ERRORE
+	  }
+	  
+	  SemDescriptorPtr* sem_descptr = sem_desc->ptr;   //Riferimento 
+	  if(!sem_descptr){//MESS ERRORE
+	  }
+	  
+	  
+	  
+	  List_Detach(&running->sem_descriptors, (ListItem*) sem_desc); //Elimino il descrittore dalla lista del processo
+	  
+	  check = SemDescriptor_free(sem_desc);						//Rilascio le risorse del Sem_Descriptor	
+	  if(!check){//MESS ERRORE
+	  }
+	  
+	
+	  
+	 
+	  List_Detach(&sem->descriptors, (ListItem*) sem_descptr);  //Elimino dalla lista dei descrittori del semaforo il riferimento a SemDescriptorPtr
+	  
+	  check = SemDescriptorPtr_free(sem_descptr);				//Rilascio le risorse del Sem_DescriptorPTR
+	  if(!check){//MESS ERRORE
+	  }
+	  
+	  if((sem->descriptors).size ==  0){ //!! && (sem->waiting_descriptors).size == 0){ DA RICONTROLLARE
+		   List_detach(&semaphores_list,(ListItem*)sem);
+			check = Semaphore_free(sem);						//Rilascio le risorse del Semaforo solo se non ci sono più descrittori associati
+			 if(!check){//MESS ERRORE
+					  
+				}	  
+	  } 
+	
+    
+    running->syscall_retvalue = 0;    //RESTITUISCO 0 nella syscall_retvalue
+	return;
+	  
+	  
+
+  
 }
