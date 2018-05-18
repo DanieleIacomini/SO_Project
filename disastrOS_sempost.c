eb225fc0ff56421f025b65f7436a0781c7b9acc9
Daniele Iacomini <root@Daniele-UB.localdomain>
2018-05-18 16:24:03 +0200

    sempost ok

--- disastrOS_sempost.c
+++ disastrOS_sempost.c
@@ -7,5 +7,27 @@
 #include "disastrOS_semdescriptor.h"
 
 void internal_semPost(){
-  // do stuff :)
+  int fd = running -> syscall_args[0];
+  SemDescriptor* des = SemDescriptorList_byFd(&running->sem_descriptors,fd);
+  if(!des) {
+    running -> syscall_retvalue = DSOS_SEMNOTFD;
+  }
+
+  Semaphore* sem = des -> semaphore;
+  if(!sem) {
+    running -> syscall_retvalue = DSOS_SEMNOTSEM;
+  }
+
+  SemDescriptorPtr* des_ptr;
+  (sem -> count)++;
+
+  if(sem->count <= 0) {
+    List_insert(&ready_list, ready_list.last, (ListItem*) running);
+    des_ptr = (SemDescriptorPtr*)List_detach(&sem->waiting_descriptors, (ListItem*) sem->waiting_descriptors.first);
+    List_insert(&sem->descriptors, sem->descriptors.last, (ListItem*) proc_desptr);
+    List_detach(&waiting_list, (ListItem*) proc_desptr->descriptor->pcb);
+    running->status = Ready;
+    running = proc_desptr->descriptor->pcb;
+  }
+
 }
